05c8ec21fc98aa2cccea1dec23ab8bc87d7e43d5
Markus Glau <57915992+glaumarkus@users.noreply.github.com>
2020-02-28 14:11:49 +0100

    Update clustering.h

--- src/clustering.h
+++ src/clustering.h
@@ -66,7 +66,7 @@ static std::vector<std::vector<int>> euclideanCluster(const std::vector<std::vec
 }
 
 
-void render_obs_box(visualization::PCLVisualizer::Ptr& viewer, PointCloud<PointXYZ>::Ptr cloud, std::string name){
+void render_obs_box(visualization::PCLVisualizer::Ptr& viewer, PointCloud<PointXYZ>::Ptr& cloud, std::string name, std::vector<int> c){
     // Find bounding box for the road
     PointXYZ minPoint, maxPoint;
     getMinMax3D(*cloud, minPoint, maxPoint);
@@ -74,29 +74,15 @@ void render_obs_box(visualization::PCLVisualizer::Ptr& viewer, PointCloud<PointX
     Box box;
     box.x_min = minPoint.x;
     box.y_min = minPoint.y;
-    box.z_min = minPoint.z;
+    box.z_min = minPoint.z - 0.4;
     box.x_max = maxPoint.x;
     box.y_max = maxPoint.y;
     box.z_max = maxPoint.z;
   
-    float obj_length = maxPoint.x - minPoint.x;
-    float obj_width = maxPoint.y - minPoint.y;
-    float obj_height = maxPoint.z - minPoint.z;
-  
-    std::vector<int> c;
-    // color based on properties
-    if (obj_length > 1.3 || obj_width > 1.3){
-      //likely car
-      c = {1,0,0};
-    }
-    else if((obj_length > .75 || obj_width > .75) && obj_height > 1.3){
-      //likely bike / pedestrian
-      c = {1,1,0};
-    }
-    else {
-      //likely obstacle
-      c = {0,0,1};
-    }
+  	// render 
+    viewer->addPointCloud<PointXYZ> (cloud, name +"cloud");
+    viewer->setPointCloudRenderingProperties (visualization::PCL_VISUALIZER_POINT_SIZE, 4, name +"cloud");
+    viewer->setPointCloudRenderingProperties (visualization::PCL_VISUALIZER_COLOR, c[0], c[1], c[2], name +"cloud");
   
     viewer->addCube(box.x_min, box.x_max, box.y_min, box.y_max, box.z_min, box.z_max, c[0], c[1],c[2], name);
     viewer->setShapeRenderingProperties(visualization::PCL_VISUALIZER_REPRESENTATION, visualization::PCL_VISUALIZER_REPRESENTATION_WIREFRAME, name); 
@@ -128,9 +114,9 @@ void render_cluster(visualization::PCLVisualizer::Ptr& viewer, PointCloud<PointX
 
     std::vector<std::vector<int>> clusterIndices = euclideanCluster(pts, tree);
 
-    for (auto& clusterIndex : clusterIndices) {
+    for (const auto& clusterIndex : clusterIndices) {
         PointCloud<PointXYZ>::Ptr obs_cluster (new PointCloud<PointXYZ>);
-        for (auto& pointIndex : clusterIndex) {
+        for (const auto& pointIndex : clusterIndex) {
             obs_cluster->points.push_back (obs->points[pointIndex]);
         }
         obs_cluster->width = obs_cluster->points.size();
@@ -141,11 +127,11 @@ void render_cluster(visualization::PCLVisualizer::Ptr& viewer, PointCloud<PointX
     }
 
     // render all clusters
-    
     int cluster_id = 0;
+    std::vector<std::vector<int>> colors = {{1,0,0}, {0,0,1}, {1,1,0}, {1,0,1}, {0,1,1}};
     for(auto cluster: clusters)
     {
-        render_obs_box(viewer, cluster, "obs"+std::to_string(cluster_id));
+        render_obs_box(viewer, cluster, "obs"+std::to_string(cluster_id), colors[cluster_id%5]);
         cluster_id++;
     }
     log("Found Clusters: " + to_string(cluster_id));
